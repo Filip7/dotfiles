29a6579e18d4803dca26c67622083c4586acdd18
FMilkovic <FMilkovic@conet.hr>
2025-10-10 12:23:16 +0200

    lazyvim: move all colorscheme config to colorschemes.lua
    Set catppuccin to transparent

--- dot_config/nvim/lua/plugins/colorschemes.lua
+++ dot_config/nvim/lua/plugins/colorschemes.lua
@@ -7,10 +7,28 @@ return {
   },
   {
     "EdenEast/nightfox.nvim",
+    opts = {
+      transparent = true,
+    },
   },
   {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+    opts = {
+      transparent_background = true,
+      float = {
+        transparent = true,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+      -- colorscheme = "catppuccin-macchiato",
+      -- colorscheme = "catppuccin-latte", -- for rare ocasions when I want to use the light theme
+      -- colorscheme = "carbonfox",
+    },
   },
 }
