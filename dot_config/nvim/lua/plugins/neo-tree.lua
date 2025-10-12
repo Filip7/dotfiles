ba00cff04474ee2e8d9b2d63e97d422dbf9cbe00
Filip M <filipm7@protonmail.com>
2025-10-12 13:13:22 +0200

    lazyvim(neo-tree): group empty dirs

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -2,6 +2,10 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     filesystem = {
+      -- group_empty_dirs and scan_mode make working with java projects
+      -- managable, basically enabling skiping all the empty package dirs
+      group_empty_dirs = true,
+      scan_mode = "deep",
       filtered_items = {
         visible = true,
       },
